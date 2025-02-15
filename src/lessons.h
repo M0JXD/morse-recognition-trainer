adc2f2227c4fc7d01119f012eadd8755ed6af21e
M0JXD <jamiedrinkell@gmail.com>
2025-02-15 03:18:58 +0000

    Slightly improve icon

--- src/lessons.h
+++ src/lessons.h
@@ -6,6 +6,6 @@ void UpdateLesson(int characterDetected);  // Passes the last pressed character 
 int UpdateLevel(int kochLetterToUpdate); // Returns newly activated letter, otherwise NOT_LETTER
 void RegressLevel(int kochLetterToRegress); 
 void ResetDueToZeroLevel(int letterThatReachedZero);
-int GetRandomActiveLetter(int mode, int introducedLetter);  // Mode specifies whether to be random for Everything mode of 
+int GetRandomActiveLetter(int mode, int introducedLetter);  // Mode specifies whether to be random for Everything mode 
 
 #endif
